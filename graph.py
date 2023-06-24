7aad5de32861932e4716bb7b7fba543294c16abe
jjfork <jakubiakrjulian@gmail.com>
2023-06-24 22:00:00 +0200

    more accurate params

--- graph.py
+++ graph.py
@@ -7,21 +7,21 @@ class Graph:
     def __init__(self):
         self.graph = {}
 
-    def add_edge(self, first_cord, connection_cord):
-        if first_cord not in self.graph:
-            self.graph[first_cord] = []
-        self.graph[first_cord].append(connection_cord)
+    def add_edge(self, first_node, connection_cord):
+        if first_node not in self.graph:
+            self.graph[first_node] = []
+        self.graph[first_node].append(connection_cord)
 
-    def dfs(self, first_cord):
+    def dfs(self, first_node):
         visited = set()
-        self.dfs_util(first_cord, visited)
+        self.dfs_util(first_node, visited)
 
-    def dfs_util(self, first_cord, visited):
-        visited.add(first_cord)
-        print(first_cord, end=' ')
+    def dfs_util(self, first_node, visited):
+        visited.add(first_node)
+        print(first_node, end=' ')
 
-        if first_cord in self.graph:
-            for neighbor in self.graph[first_cord]:
+        if first_node in self.graph:
+            for neighbor in self.graph[first_node]:
                 if neighbor not in visited:
                     self.dfs_util(neighbor, visited)
 
@@ -42,9 +42,9 @@ class Graph:
 
     def transpose(self):
         transposed_graph = Graph()
-        for connection_cord in self.graph:
-            for first_cord in self.graph[connection_cord]:
-                transposed_graph.add_edge(first_cord, connection_cord)
+        for connection_node  in self.graph:
+            for first_node in self.graph[connection_node]:
+                transposed_graph.add_edge(first_node, connection_node)
         return transposed_graph
 
     def kosaraju(self):
@@ -52,29 +52,29 @@ class Graph:
         visited = set()
         scc_list = []
 
-        for first_cord in self.graph:
-            if first_cord not in visited:
-                self.kosaraju_util(first_cord, visited, stack)
+        for first_node in self.graph:
+            if first_node not in visited:
+                self.kosaraju_util(first_node, visited, stack)
                 stack.append(None)  # Mark the end of a component
 
         transposed_graph = self.transpose()
         visited = set()
 
         while stack:
-            first_cord = stack.pop()
-            if first_cord is not None and first_cord not in visited:
+            first_node = stack.pop()
+            if first_node is not None and first_node not in visited:
                 scc = []
-                transposed_graph.kosaraju_util(first_cord, visited, scc)
+                transposed_graph.kosaraju_util(first_node, visited, scc)
                 scc_list.append(scc)
 
         return scc_list
 
-    def kosaraju_util(self, first_cord, visited, scc):
-        visited.add(first_cord)
-        scc.append(first_cord)
+    def kosaraju_util(self, first_node, visited, scc):
+        visited.add(first_node)
+        scc.append(first_node)
 
-        if first_cord in self.graph:
-            for neighbor in self.graph[first_cord]:
+        if first_node in self.graph:
+            for neighbor in self.graph[first_node]:
                 if neighbor not in visited:
                     self.kosaraju_util(neighbor, visited, scc)
 
@@ -86,9 +86,9 @@ class Graph:
 
     def display_graph(self):
         g = nx.DiGraph()
-        for connection_cord in self.graph:
-            for first_cord in self.graph[connection_cord]:
-                g.add_edge(connection_cord, first_cord)
+        for connection_node in self.graph:
+            for first_node in self.graph[connection_node]:
+                g.add_edge(connection_node, first_node)
 
         pos = nx.spring_layout(g)
         nx.draw(g, pos, with_labels=True)
@@ -115,8 +115,8 @@ if __name__ == '__main__':
     print("\n^^^^^^^^BFS")
 
     print("Silnie spójne składowe:")
-    scc = g.kosaraju()
-    for component in scc:
+    korasaj = g.kosaraju()
+    for component in korasaj:
         print(component)
     print("\n")
 
